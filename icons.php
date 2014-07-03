bd21b3bd0e66656b389df51cba666f7fdc3c6678
SherryTheIntern <camworld@UXDesign1.local>
2014-07-03 12:56:53 -0400

    second commit
    complete start over from Flat UI

---
+++ icons.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Icons - UX Sandbox</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    
+
+    <!-- Loading Flat UI -->
+
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Loading UBS stylesheets -->
+    <link rel="stylesheet" href="css/UIframwork.css" />
+    <link rel="stylesheet" href="css/ir-styles.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/global.css" />
+
+    <!-- Loading UBS Javascript--> 
+    <script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="js/ir-behaviors.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <?php include("_includes/nav.html");?>
+    <?php include("_includes/icons.html");?>
+
+    
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/typeahead.js"></script>
+    <script src="bootstrap/js/google-code-prettify/prettify.js"></script>
+    <script src="js/application.js"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+    <script src="js/typeahead.js"></script>
+    <script src="bootstrap/js/google-code-prettify/prettify.js"></script>
+    <script src="js/application.js"></script>
+  </body>
+</html>
