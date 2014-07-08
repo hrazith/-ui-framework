5efe6e837a804cd1607e05ad8e0700313787e0c1
SherryTheIntern <camworld@UXDesign1.local>
2014-07-08 16:33:20 -0400

    added new pages
    filters
    footers
    headers
    megamenu
    typography

---
+++ footers.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Containers - UX Sandbox</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading UBS stylesheets -->
+    <link rel="stylesheet" href="css/global.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/ir-styles.css" />
+    <link rel="stylesheet" href="css/UIframwork.css" />
+
+    <!-- Loading UBS Javascript--> 
+    <script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="js/ir-behaviors.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  
+  </head>
+  <body>
+    
+    <?php include("_includes/nav.html");?>
+    <?php include("_includes/footers.html");?>
+
+
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/typeahead.js"></script>
+    <script src="js/google-code-prettify/prettify.js"></script>
+    <script src="js/application.js"></script>
+  </body>
+</html>
