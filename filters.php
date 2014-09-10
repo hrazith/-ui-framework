7bd6b18c6b55778101cc38c8762aec16d9d0af13
Sherry Icy <SherryAli@Icys-MacBook-Pro.local>
2014-09-10 16:01:07 -0400

    adding in coding tabs

--- filters.php
+++ filters.php
@@ -10,12 +10,14 @@
 <link rel="stylesheet" href="styles/ir-styles.css" />
 <link rel="stylesheet" href="styles/styles.css" />
 <link rel="stylesheet" href="styles/UIframwork.css" />
+<link rel="stylesheet" href="styles/prism.css" />
 
     <!-- Loading UBS Javascript--> 
     <script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
     <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
     <script type="text/javascript" src="js/ir-behaviors.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
+    <script src="js/prism.js"></script>
   
   </head>
   <body>
@@ -28,7 +30,6 @@
     <script src="js/jquery-1.8.3.min.js"></script>
     <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
     <script src="js/typeahead.js"></script>
-    <script src="js/google-code-prettify/prettify.js"></script>
     <script src="js/application.js"></script>
   </body>
 </html>
