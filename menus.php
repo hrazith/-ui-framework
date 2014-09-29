5b2c7e5cca24fb5d379952828de72aded0f14089
Sherry Icy <SherryAli@Icys-MacBook-Pro.local>
2014-09-29 14:55:45 -0400

    #18 menus
    change to menus
    keep megamenu
    add topnav (from headers) and call it breadcrumbs
    
    —done

--- menus.php
+++ menus.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Megamenus - UX Sandbox</title>
+    <title>Menus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Loading UBS stylesheets -->
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href="styles/ir-styles.css" />
 <link rel="stylesheet" href="styles/styles.css" />
 <link rel="stylesheet" href="styles/UIframwork.css" />
+<link rel="stylesheet" href="styles/prism.css" />
 
 
     <!-- Loading UBS Javascript--> 
@@ -17,12 +18,13 @@
     <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
     <script type="text/javascript" src="js/ir-behaviors.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
+    <script src="js/prism.js"></script>
   
   </head>
   <body>
     
     <?php include("_includes/nav.html");?>
-    <?php include("_includes/megamenus.html");?>
+    <?php include("_includes/menus.html");?>
 
 
     <!-- Load JS here for greater good =============================-->
