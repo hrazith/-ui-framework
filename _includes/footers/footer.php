ffa9e97cfa6fe665f87f75a77bf17dc60f0a8e7e
Sherry Icy <SherryAli@Icys-MacBook-Pro.local>
2014-11-07 13:26:48 -0500

    edits

--- _includes/footers/footer.php
+++ _includes/footers/footer.php
@@ -1,3 +1,50 @@
+
 <div class="row bmss-footer">
 	<p class="light-blue">&copy; 2013 Branch Manager Supervisory System</p>
 </div>
+
+<br />
+<!-- *************************
+        Coding Tabs 
+***************************-->
+<div class="tabs">
+    <ul class="tab-links">
+        <li class="active"><a href="#tab1">HTML</a></li>
+        <li><a href="#tab2">CSS</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="tab1" class="tab active">
+            <p>
+                <pre class="language-markup">
+                    <code>
+                      <!--HTML-->
+                    </code>
+                </pre>
+            </p>
+        </div><!-- /tab1-->
+     
+        <div id="tab2" class="tab">
+            <p>
+                <pre class="language-css">
+                    <code>
+.bmss-footer {
+    border-top: 1px solid #d7d7d7;
+    width: 978px;
+    height: 52px; 
+    margin: 0px auto;
+}
+
+.bmss-footer p {
+    padding: 12px;
+}
+                    </code>
+                </pre>
+            </p>
+        </div><!-- /tab2-->
+
+    </div><!-- /tab-content-->
+
+</div><!-- /tabs-->
+ 
+
